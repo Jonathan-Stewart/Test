673f993410eaa2bc3a27dfcc4c4728d696f18ed7
Jonathan Stewart <P14167237@GWHXX582LIN015W.lec-admin.dmu.ac.uk>
2016-01-26 15:21:27 +0000

    Fixed code

--- helloWorld.c
+++ helloWorld.c
@@ -3,15 +3,3 @@ main()
 {    
    printf("Hello World\n");
 }
- 
-
-Save, add and commit helloWorld.c with an appropriate message.
-
-Now modify the program so it is reads
-
-#include<stdio.h>  
-main() 
-{     
-   printf("Hello World\n");
-   printf(�Hello You!\n�);
-}
